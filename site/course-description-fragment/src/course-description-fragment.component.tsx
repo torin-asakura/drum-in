fedba70f69ecb16a3f11cfc89a07dd6a22e4a217
Aleksandr Zavadkin <gochicus@atls.team>
2021-11-29 09:33:02 +0000

    feature(course-description-fragment):new mobile markup

--- site/course-description-fragment/src/course-description-fragment.component.tsx
+++ site/course-description-fragment/src/course-description-fragment.component.tsx
@@ -12,50 +12,58 @@ import { Button }   from '@ui/button'
 const CourseDescription: FC = () => {
   // TODO Modal appearacnce logic
   return (
-    <Box width='1440px' border='1px solid black'>
-      <Layout flexBasis={137} />
+    <Box width={['320px','320px','1440px']} height={['290px','290px','722px']} border='1px solid black'>
+      <Layout flexBasis={[20,20,137]} />
       <Row>
-        <Column>
-          <Layout flexBasis={53} />
-          <Layout>
+        <Column alignItems='center'>
+          <Layout flexBasis={[25,25,53]} />
+          <Layout display={['none','none','flex']}>
             <Text>course</Text>
           </Layout>
-          <Layout flexBasis={30} />
+          <Layout flexBasis={30}  display={['none','none','flex']}/>
           <Layout>
-            <Text>rithm</Text>
+            <Text>rythm</Text>
           </Layout>
-          <Layout flexBasis={39} />
-          <Box width='572px' height='311px' border='1px solid black'>
+          <Layout flexBasis={[30,30,39]}  />
+          <Box width={['280px','280px','572px']} height={['145px','145px','311px']} border='1px solid black'>
             <Layout>
               <Text>text</Text>
             </Layout>
           </Box>
-          <Layout flexBasis={70} />
-          <Layout>
+          <Layout flexBasis={24} flexShrink={0} display={['flex','flex', 'none']}/>
+          <Row justifyContent='center' display={['flex','flex','none']}>
+            <Layout width='26px' height='6px'>
+              <Box width='3px' height='3px' border='1px solid black' borderRadius={50} />
+              <Layout flexBasis={10} flexShrink={0} />
+              <Box width='3px' height='3px' border='1px solid black' borderRadius={50} />
+            </Layout>
+          </Row>
+          <Layout flexBasis={34} flexShrink={0} display={['flex','flex', 'none']}/>
+          <Layout flexBasis={70}  display={['none','none','flex']}/>
+          <Layout display = {['none','none','flex']}>
             <Text>course description</Text>
           </Layout>
-          <Layout flexBasis={60} />
+          <Layout flexBasis={60} display={['none','none','flex']} />
           <Button
-            onClick={() => setShow('flex')}
             border='1px solid black'
-            width='426px'
-            height='70px'
+            width={['237px','237px','426px']}
+            height={['41px','41px','70px']}
           >
             <Layout>
               <Text>sign up</Text>
             </Layout>
           </Button>
-          <Layout flexBasis={60} flexShrink={0} />
+          <Layout flexBasis={60} flexShrink={0} display={['none','none','flex']}/>
         </Column>
-        <Layout flexBasis={150} flexShrink={0} />
+        <Layout flexBasis={150} flexShrink={0}  display={['none','none','flex']}/>
         <Column>
           <Layout flexBasis={65} />
-          <Box width='494px' height='596px' border='1px solid black'>
+          <Box width='494px' height='596px' border='1px solid black' display={['none','none','flex']}>
             <Layout>photo</Layout>
           </Box>
         </Column>
       </Row>
-      <Layout flexBasis={118} />
+      <Layout flexBasis={[20,20,118]} />
     </Box>
   )
 }
--- site/course-description-fragment/src/index.ts
+++ site/course-description-fragment/src/index.ts
@@ -1,2 +1,1 @@
 export * from './course-description-fragment.component'
-export * from './mobile'
