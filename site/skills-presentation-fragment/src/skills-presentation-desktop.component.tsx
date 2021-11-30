7d1df634f716cbc85094ec1390d26e071aa94e76
Aleksandr Zavadkin <gochicus@atls.team>
2021-11-30 11:57:12 +0000

    refactor(site): mobile and desktop markup split

---
+++ site/skills-presentation-fragment/src/skills-presentation-desktop.component.tsx
@@ -0,0 +1,95 @@
+import React from 'react'
+
+import { FC }     from 'react'
+
+import { Box }    from '@ui/layout'
+import { Column } from '@ui/layout'
+import { Layout } from '@ui/layout'
+import { Row }   from '@ui/layout'
+
+const SkillsPresentationDesktop:FC = () => 
+<Column width='100%' display={['none', 'none', 'flex']}>
+        <Layout flexBasis={65} />
+        <Row>
+          <Column>
+            <Layout flexBasis={262} />
+            <Box width='115px' height='60px' border='1px solid black' />
+          </Column>
+          <Layout flexBasis={45} flexShrink={0} />
+          <Column justifyContent='center' width='100%'>
+            <Row>
+              <Box width='68px' height='58px' border='1px solid black'>
+                <Layout>img</Layout>
+              </Box>
+              <Layout flexBasis={36} />
+              <Box width='320px' border='1px solid black'>
+                <Layout>text</Layout>
+              </Box>
+            </Row>
+            <Layout flexBasis={62} />
+            <Row>
+              <Box width='68px' height='58px' border='1px solid black'>
+                <Layout>img</Layout>
+              </Box>
+              <Layout flexBasis={36} />
+              <Box width='320px' border='1px solid black'>
+                <Layout>text</Layout>
+              </Box>
+            </Row>
+            <Layout flexBasis={62} />
+            <Row>
+              <Box width='68px' height='58px' border='1px solid black'>
+                <Layout>img</Layout>
+              </Box>
+              <Layout flexBasis={36} />
+              <Box width='320px' border='1px solid black'>
+                <Layout>text</Layout>
+              </Box>
+            </Row>
+          </Column>
+          <Column justifyContent='center' width='100%'>
+            <Row justifyContent='flex-end'>
+              <Box width='68px' height='58px' border='1px solid black'>
+                <Layout>img</Layout>
+              </Box>
+              <Layout flexBasis={36} />
+              <Box width='320px' border='1px solid black'>
+                <Layout>text</Layout>
+              </Box>
+            </Row>
+            <Layout flexBasis={62} />
+            <Row justifyContent='flex-end'>
+              <Box width='68px' height='58px' border='1px solid black'>
+                <Layout>img</Layout>
+              </Box>
+              <Layout flexBasis={36} />
+              <Box width='320px' border='1px solid black'>
+                <Layout>text</Layout>
+              </Box>
+            </Row>
+            <Layout flexBasis={62} />
+            <Row justifyContent='flex-end'>
+              <Box width='68px' height='58px' border='1px solid black'>
+                <Layout>img</Layout>
+              </Box>
+              <Layout flexBasis={36} />
+              <Box width='320px' border='1px solid black'>
+                <Layout>text</Layout>
+              </Box>
+            </Row>
+          </Column>
+          <Layout flexBasis={200} flexShrink={0} />
+        </Row>
+        <Layout flexBasis={68} />
+        <Row justifyContent='center'>
+          <Box width='68px' height='58px' border='1px solid black'>
+            <Layout>img</Layout>
+          </Box>
+          <Layout flexBasis={36} />
+          <Box width='320px' border='1px solid black'>
+            <Layout>text</Layout>
+          </Box>
+        </Row>
+      </Column>
+
+export {SkillsPresentationDesktop}
