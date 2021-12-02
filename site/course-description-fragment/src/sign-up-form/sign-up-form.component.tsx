e015a11faff599cedb79f63289a39aabda9da881
Aleksandr Zavadkin <gochicus@atls.team>
2021-12-02 09:48:39 +0000

    chore(course-description-fragment): misspelling fixed

--- site/course-description-fragment/src/sign-up-form/sign-up-form.component.tsx
+++ site/course-description-fragment/src/sign-up-form/sign-up-form.component.tsx
@@ -9,17 +9,17 @@ import { Input }  from '@ui/input'
 const SignUpFrom: FC = () => (
   <Box width='611px' height='461px' border='1px solid black'>
     <Layout flexBasis={92} />
-    <Column style={{ border: '1pxsolid black' }}>
+    <Column style={{ border: '1px solid black' }}>
       <Layout flexBasis={64} />
-      <Layout style={{ border: '1pxsolid black' }}>
+      <Layout style={{ border: '1px solid black' }}>
         <Input />
       </Layout>
       <Layout flexBasis={64} />
-      <Layout style={{ border: '1pxsolid black' }}>
+      <Layout style={{ border: '1px solid black' }}>
         <Input />
       </Layout>
       <Layout flexBasis={64} />
-      <Layout style={{ border: '1pxsolid black' }} width='426px' height='70px'>
+      <Layout style={{ border: '1px solid black' }} width='426px' height='70px'>
         sign up
       </Layout>
       <Layout flexBasis={64} />
