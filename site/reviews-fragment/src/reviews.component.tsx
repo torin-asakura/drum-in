9fd4a097757b63dce83971b9e56ca679a6eeaf12
Aleksandr Zavadkin <gochicus@atls.team>
2021-12-06 12:39:33 +0000

    refactor(reviews-fragment): formatting changed

--- site/reviews-fragment/src/reviews.component.tsx
+++ site/reviews-fragment/src/reviews.component.tsx
@@ -14,9 +14,7 @@ const Reviews: FC = () => (
   >
     <Layout flexBasis={138} flexShrink={0} display={['none', 'none', 'flex']} />
     <Column width='100%' display={['none', 'none', 'flex']} style={{ border: '1px solid black' }}>
-      <Layout style={{ border: '1px solid black' }}>
-         text
-      </Layout>
+      <Layout style={{ border: '1px solid black' }}>text</Layout>
       <Layout flexBasis={56} flexShrink={0} />
       <Row style={{ border: '1px solid black' }}>
         <Layout width='527px' height='284px' style={{ border: '1px solid black' }}>
@@ -34,11 +32,11 @@ const Reviews: FC = () => (
       <Layout flexBasis={10} />
       <Column width='100%' style={{ border: '1px solid black' }}>
         <Layout flexBasis={15} />
-        <Layout style={{ border: '1px solid black' }}>
-          reviews
-        </Layout>
+        <Layout style={{ border: '1px solid black' }}>reviews</Layout>
         <Layout flexBasis={15} />
-        <Layout width={300} height={160} style={{ border: '1px solid black' }}>youtube player</Layout>
+        <Layout width={300} height={160} style={{ border: '1px solid black' }}>
+          youtube player
+        </Layout>
         <Layout flexBasis={15} />
       </Column>
       <Layout flexBasis={10} />
