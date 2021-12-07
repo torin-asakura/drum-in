c204f8a99342bc1adda8532754d148f8841c95a0
Aleksandr Zavadkin <gochicus@atls.team>
2021-12-07 13:24:21 +0000

    feature(portfolio-fragment): intl messages added

---
+++ site/reviews-fragment/src/messages.ts
@@ -0,0 +1,10 @@
+import { defineMessages } from 'react-intl'
+
+import { name }           from '../package.json'
+
+export default defineMessages({
+  reviews: {
+    id: `${name}.reviews`,
+    defaultMessage: 'отзывы',
+  },
+})
