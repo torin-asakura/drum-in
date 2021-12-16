943a6060031982917dd05c2fbc9b466e8ea9ca89
Aleksandr Zavadkin <gochicus@atls.team>
2021-12-16 09:24:10 +0000

    refactor(skills-to-learn-fragment): List component render condition check change

--- site/portfolio-fragment/src/portfolio.component.tsx
+++ site/portfolio-fragment/src/portfolio.component.tsx
@@ -8,55 +8,56 @@ import { Layout }     from '@ui/layout'
 import { Row }        from '@ui/layout'
 import { ImageBlock } from '@ui/image'
 import { Text }       from '@ui/text'
-import { genres }       from '@site/data'
+import { genres }     from '@site/data'
 
-const Portfolio: FC = () => 
-    <Box width='100%' height='1100px' display={['none', 'none', 'flex']} justifyContent='center'>
-      <Layout width='1440px'>
-        <Column width='100%'>
-          <Row justifyContent='space-between'>
-            <Layout flexBasis={65} flexShrink={0} />
-            <Column alignItems='center'>
-              <Layout flexBasis={70} />
-              <Box width='355px' height='64px' backgroundColor='dullRed' />
-              <Layout width='505px' height='392px'>
-                <ImageBlock />
-              </Layout>
-              <Box width='355px' height='74px' backgroundColor='dullRed' />
-              <Layout width='505px' height='392px'>
-                <ImageBlock />
-              </Layout>
-              <Box width='355px' height='45px' backgroundColor='dullRed' />
-            </Column>
-            <Layout flexBasis={160} flexShrink={0} />
-            <Column>
-              <Layout flexBasis='126px' />
-              <Layout>
-                <Column>
-                  {genres.map((item) => (
-                    <>
-                      <Layout>
-                        <Text
-                          color='dullBlack'
-                          fontWeight='normal'
-                          fontSize='normal'
-                          lineHeight='normal'
-                        >
-                          {item}
-                        </Text>
-                      </Layout>
-                      <Layout flexBasis={40} />
-                    </>
-                  ))}
-                </Column>
-              </Layout>
-              <Layout flexBasis={82} />
-            </Column>
-            <Layout flexBasis={143} flexShrink={0} />
-          </Row>
-          <Layout flexBasis={60} />
-        </Column>
-      </Layout>
-    </Box>
+const Portfolio: FC = () => (
+  <Box width='100%' height='1100px' display={['none', 'none', 'flex']} justifyContent='center'>
+    <Layout width='1440px'>
+      <Column width='100%'>
+        <Row justifyContent='space-between'>
+          <Layout flexBasis={65} flexShrink={0} />
+          <Column alignItems='center'>
+            <Layout flexBasis={70} />
+            <Box width='355px' height='64px' backgroundColor='dullRed' />
+            <Layout width='505px' height='392px'>
+              <ImageBlock />
+            </Layout>
+            <Box width='355px' height='74px' backgroundColor='dullRed' />
+            <Layout width='505px' height='392px'>
+              <ImageBlock />
+            </Layout>
+            <Box width='355px' height='45px' backgroundColor='dullRed' />
+          </Column>
+          <Layout flexBasis={160} flexShrink={0} />
+          <Column>
+            <Layout flexBasis='126px' />
+            <Layout>
+              <Column>
+                {genres.map((item) => (
+                  <>
+                    <Layout>
+                      <Text
+                        color='dullBlack'
+                        fontWeight='normal'
+                        fontSize='normal'
+                        lineHeight='normal'
+                      >
+                        {item}
+                      </Text>
+                    </Layout>
+                    <Layout flexBasis={40} />
+                  </>
+                ))}
+              </Column>
+            </Layout>
+            <Layout flexBasis={82} />
+          </Column>
+          <Layout flexBasis={143} flexShrink={0} />
+        </Row>
+        <Layout flexBasis={60} />
+      </Column>
+    </Layout>
+  </Box>
+)
 
 export { Portfolio }
--- site/skills-to-learn-fragment/src/skills/item/list.component.tsx
+++ site/skills-to-learn-fragment/src/skills/item/list.component.tsx
@@ -1,21 +1,23 @@
-import React      from 'react'
+import React       from 'react'
 
-import { Layout } from '@ui/layout'
-import { Column } from '@ui/layout'
-import { Text }   from '@ui/text'
+import { Layout }  from '@ui/layout'
+import { Column }  from '@ui/layout'
+import { Text }    from '@ui/text'
+import { Condition } from '@ui/condition'
 
-const List = ({ mainText }) =>
-  mainText !== !mainText
-    ? mainText.map((item) => (
-        <Column>
-          <Layout>
-            <Text color='dullBlack' fontWeight='normal' fontSize='normal' lineHeight='normal'>
-              {item}
-            </Text>
-          </Layout>
-          <Layout flexBasis={20} />
-        </Column>
-      ))
-    : null
+const List = ({ mainText }) => (
+  <Condition match={Array.isArray(mainText)}>
+    {mainText.map((item) => (
+      <Column>
+        <Layout>
+          <Text color='dullBlack' fontWeight='normal' fontSize='normal' lineHeight='normal'>
+            {item}
+          </Text>
+        </Layout>
+        <Layout flexBasis={20} />
+      </Column>
+    ))}
+  </Condition>
+)
 
 export { List }
