25e341f021c2611e22310f20e22f524a15a6e607
Aleksandr Zavadkin <gochicus@atls.team>
2021-12-23 16:54:52 +0000

    feature(button): button appearence styles

--- ui/button/src/button.styles.ts
+++ ui/button/src/button.styles.ts
@@ -1,7 +1,35 @@
-import { getBaseStyles }            from './styles'
-import { getShapeStyles }           from './styles'
-import { getAppearanceColorStyles } from './styles'
+import { CSSObject }              from '@emotion/css'
+import { createBaseStyles }       from '@atls-ui-parts/button'
+import { createAppearanceStyles } from '@atls-ui-parts/button'
+import { createShapeStyles }      from '@atls-ui-parts/button'
 
-export const baseStyles = getBaseStyles()
-export const shapeStyles = getShapeStyles()
-export const appearanceStyles = getAppearanceColorStyles()
+export const baseStyles = createBaseStyles()
+
+export const baseSelectStyles: CSSObject = {
+  position: 'relative',
+  width: '100%',
+  justifyContent: 'flex-start',
+}
+
+export const appearanceStyles = createAppearanceStyles({
+  fontColor: ({ theme }) =>  theme.colors.dullRed,
+  backgroundColor: ({ theme }) => theme.colors.dullRed,
+  borderColor: ({ theme }) => theme.colors.dullRed,
+})
+
+export const hoverAppearanceStyles = (props) => ({
+  '&:hover': createAppearanceStyles({
+    fontColor: ({ theme }) =>
+   theme.colors.dullRed,
+    backgroundColor: ({ theme }) => theme.colors.duskRed,
+    borderColor: ({ theme }) => theme.colors.duskRed,
+  })(props),
+})
+
+export const shapeStyles = createShapeStyles({
+  size: 56,
+  fontSize: ({ theme }) => theme.fontSizes.default,
+  rounding: (({ theme }) => theme.radii.intermediate) as any,
+  paddingLeft: 16,
+  paddingRight: 16,
+})
--- ui/button/src/styles/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './appearance'
-export * from './base'
-export * from './shape'
