a934ff7481b4887ee3931a4cdb2fa879f48cea43
Nikita Elfimov <me@tfk.name>
2023-01-24 02:16:13 +0500

    chore: horizontal parallax scroll

--- landing/fragments/landing-program-horizontal/src/program-horizontal.component.tsx
+++ landing/fragments/landing-program-horizontal/src/program-horizontal.component.tsx
@@ -6,8 +6,8 @@ import { Box }              from '@ui/layout'
 const ProgramHorizontalBlock = () => {
 
   return (
-   <Box position='relative'>
-     <Box data-scroll data-scroll-direction="horizontal">
+   <Box position='relative' id='scroll'>
+     <Box data-scroll data-scroll-direction="horizontal" data-scroll-speed='9' data-scroll-target='#scroll'>
        <Box flexShrink={0} width={1000} height='100vh' backgroundColor='orange' />
        <Box flexShrink={0} width={1000} height='100vh' backgroundColor='pink' />
        <Box flexShrink={0} width={1000} height='100vh' backgroundColor='green' />
