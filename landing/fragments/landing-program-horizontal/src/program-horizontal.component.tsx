fed8bdf2dfbdc62d69993c0b7bdc1733d90bd715
Nikita Elfimov <me@tfk.name>
2023-02-14 02:49:34 +0500

    fix: sticky scroll

--- landing/fragments/landing-program-horizontal/src/program-horizontal.component.tsx
+++ landing/fragments/landing-program-horizontal/src/program-horizontal.component.tsx
@@ -6,11 +6,13 @@ import { Box }              from '@ui/layout'
 const ProgramHorizontalBlock = () => {
 
   return (
-   <Box position='relative' id='scroll'>
+       <Box display='block' position='relative' height={1920} id='scroll'>
+        <Box data-scroll data-scroll-sticky data-scroll-target='#scroll'>
      <Box data-scroll data-scroll-direction="horizontal" data-scroll-speed='9' data-scroll-target='#scroll'>
-       <Box flexShrink={0} width={1000} height='100vh' backgroundColor='orange' />
-       <Box flexShrink={0} width={1000} height='100vh' backgroundColor='pink' />
-       <Box flexShrink={0} width={1000} height='100vh' backgroundColor='green' />
+       <Box flexShrink={0} width={1000} height='50vh' backgroundColor='orange' />
+       <Box flexShrink={0} width={1000} height='50vh' backgroundColor='pink' />
+       <Box flexShrink={0} width={1000} height='50vh' backgroundColor='green' />
+     </Box>
      </Box>
    </Box>
   )
