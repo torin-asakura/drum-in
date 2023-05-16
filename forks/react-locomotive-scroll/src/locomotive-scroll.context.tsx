4657b18843106ed3ebd41920af20134bd840a7d2
Nelfimov <nelfimov@mail.ru>
2023-05-16 15:32:11 +0300

    fix: update tsconfig.json

--- forks/react-locomotive-scroll/src/locomotive-scroll.context.tsx
+++ forks/react-locomotive-scroll/src/locomotive-scroll.context.tsx
@@ -1,4 +1,4 @@
-import * as React                       from 'react'
+import React                       from 'react'
 import useResizeObserver           from 'use-resize-observer'
 import { LocomotiveScrollOptions } from 'locomotive-scroll'
 import { Scroll }                  from 'locomotive-scroll'
