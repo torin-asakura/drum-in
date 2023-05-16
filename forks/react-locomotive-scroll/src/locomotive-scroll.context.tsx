caa4cb9f7fa1ee4921908eb3592707a2a01c7802
Nelfimov <nelfimov@mail.ru>
2023-05-16 15:26:41 +0300

    fix: extending tsconfig.json of button

--- forks/react-locomotive-scroll/src/locomotive-scroll.context.tsx
+++ forks/react-locomotive-scroll/src/locomotive-scroll.context.tsx
@@ -1,4 +1,4 @@
-import React                       from 'react'
+import * as React                       from 'react'
 import useResizeObserver           from 'use-resize-observer'
 import { LocomotiveScrollOptions } from 'locomotive-scroll'
 import { Scroll }                  from 'locomotive-scroll'
