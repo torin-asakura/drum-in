bfffaed3f91d6f08f9803bf3b9abf6c97521272b
Nelfimov <nelfimov@mail.ru>
2023-05-17 10:32:34 +0300

    fix: seo details, based on misik

---
+++ landing/pages/index-page/src/seo.component.tsx
@@ -0,0 +1,65 @@
+import React  from 'react'
+import Helmet from 'react-helmet'
+
+const Seo = ({ language, ogCover, SEO }) => {
+  const { title } = SEO[language] || { title: 'Drum instructor' }
+
+  return (
+    <Helmet
+      title={title}
+      meta={[
+        {
+          name: 'description',
+          lang: 'ru',
+          content: SEO.RU.metaDesc,
+        },
+        {
+          name: 'description',
+          lang: 'en',
+          content: SEO.EN.metaDesc,
+        },
+        {
+          property: 'og:title',
+          content: title,
+        },
+        {
+          property: 'og:description',
+          lang: 'ru',
+          content: SEO.RU.metaDesc,
+        },
+        {
+          property: 'og:description',
+          lang: 'en',
+          content: SEO.EN.metaDesc,
+        },
+        {
+          property: 'og:image',
+          content: ogCover,
+        },
+        {
+          property: 'og:type',
+          content: 'website',
+        },
+        {
+          name: 'twitter:card',
+          content: 'summary',
+        },
+        {
+          name: 'twitter:title',
+          content: title,
+        },
+        {
+          name: 'twitter:description',
+          lang: 'ru',
+          content: SEO.RU.metaDesc,
+        },
+        {
+          name: 'twitter:description',
+          lang: 'en',
+          content: SEO.EN.metaDesc,
+        },
+      ]}
+    />
+  )
+}
+export { Seo }
