39565a805868c51696a50ce3c48053c36449a9ab
Ilya Frolov <absolem.dev@gmail.com>
2023-10-25 19:37:45 +0400

    fix: meta-tags on seven-heaven-page

---
+++ landing/pages/seventh-heaven-page/src/data/queries/seo.query.ts
@@ -0,0 +1,18 @@
+import { gql } from "@apollo/client";
+
+export const GET_SEVENTH_HEAVEN_SEO = gql`
+  query GetIndexSeo {
+    pageBy(uri: "/seventh-heaven") {
+      seo {
+        title
+        metaDesc
+      }
+      translation(language: EN) {
+        seo {
+          title
+          metaDesc
+        }
+      }
+    }
+  }
+`;
