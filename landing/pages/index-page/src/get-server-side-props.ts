4403619c2136e25507855803443013415250e447
Ilya Frolov <absolem.dev@gmail.com>
2023-10-25 19:03:10 +0400

    fix: meta-tags on index-page

--- landing/pages/index-page/src/get-server-side-props.ts
+++ landing/pages/index-page/src/get-server-side-props.ts
@@ -1,52 +1,35 @@
-import { GET_PREVIEW }         from '@globals/data'
-import { getClient }           from '@globals/data'
-import { setCacheHeader }      from '@globals/data'
+import { GET_PREVIEW } from "@globals/data";
+import { getClient } from "@globals/data";
+import { setCacheHeader } from "@globals/data";
 
-import { GET_INDEX_SEO }       from './data'
-import { runAboutQuery }       from './data'
-import { runFeedbackQuery }    from './data'
-import { runHeroQuery }        from './data'
-import { runServicesQuery }    from './data'
-import { runWorkFormatsQuery } from './data'
+import { GET_INDEX_SEO } from "./data";
 
 export const getServerSideProps = async ({ res }) => {
-  const client = getClient()
+  const client = getClient();
 
-  let SEO
+  let SEO;
 
-  setCacheHeader(res, 3600, 300)
+  setCacheHeader(res, 3600, 300);
 
   const { data: seoData } = await client.query({
     query: GET_INDEX_SEO,
-  })
+  });
 
   const { data: previewData } = await client.query({
     query: GET_PREVIEW,
     variables: {
-      uri: '/main-preview/',
+      uri: "/home-preview/",
     },
-  })
+  });
 
   if (seoData) {
     SEO = {
       RU: seoData.pageBy.seo,
       EN: seoData.pageBy.translation.seo,
-    }
-  } else SEO = { RU: {}, EN: {} }
+    };
+  } else SEO = { RU: {}, EN: {} };
 
-  const ogCover = previewData?.mediaItemBy.sourceUrl
+  const ogCover = previewData?.mediaItemBy.sourceUrl;
 
-  const queryPromises: Array<Promise<any>> = [
-    runHeroQuery(),
-    runAboutQuery(),
-    runServicesQuery(),
-    runWorkFormatsQuery(),
-    runFeedbackQuery(),
-  ]
-
-  const retrievedData = await Promise.all(queryPromises)
-
-  const data = retrievedData.reduce((props, allData) => ({ ...props, ...allData }), {})
-
-  return { props: { SEO, ogCover, data } }
-}
+  return { props: { SEO, ogCover } };
+};
