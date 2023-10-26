6affaa2c3d0cdbbf348031e3c67023ab5de5bf5d
Ilya Frolov <absolem.dev@gmail.com>
2023-10-26 20:58:09 +0400

    fix: seo component on feeling-of-time-page

--- landing/pages/feeling-of-time-page/src/data/queries/seo.query.ts
+++ landing/pages/feeling-of-time-page/src/data/queries/seo.query.ts
@@ -2,16 +2,29 @@ import { gql } from '@apollo/client'
 
 export const GET_FEELING_OF_TIME_SEO = gql`
   query GetIndexSeo {
-    pageBy(uri: "/a-sense-of-time") {
+    pageContentBy(uri: "/a-sense-of-time") {
       seo {
-        title
+        canonical
+        cornerstone
+        focuskw
+        fullHead
         metaDesc
-      }
-      translation(language: EN) {
-        seo {
-          title
-          metaDesc
-        }
+        metaKeywords
+        metaRobotsNofollow
+        metaRobotsNoindex
+        opengraphAuthor
+        opengraphDescription
+        opengraphModifiedTime
+        opengraphPublishedTime
+        opengraphPublisher
+        opengraphSiteName
+        opengraphTitle
+        opengraphType
+        opengraphUrl
+        readingTime
+        title
+        twitterDescription
+        twitterTitle
       }
     }
   }
