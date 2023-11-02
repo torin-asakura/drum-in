e12657b68ffea835cf786bf2cb5fff957881d86d
Ilya Frolov <absolem.dev@gmail.com>
2023-11-02 14:26:37 +0400

    refactor(progress-bar): init constant, fix naming

--- ui/progress-bar/src/use-progress-bar.hook.ts
+++ ui/progress-bar/src/use-progress-bar.hook.ts
@@ -1,27 +1,36 @@
-import { useEffect }           from 'react'
+import { useEffect }                       from 'react'
 
-import { useLocomotiveScroll } from '@forks/react-locomotive-scroll'
-import { theme }               from '@ui/theme'
+import { useLocomotiveScroll }             from '@forks/react-locomotive-scroll'
+import { theme }                           from '@ui/theme'
 
-export function useProgressBar(scrollId, progressRef) {
+import { HEIGHT_PATH_OF_PROGRESS_BAR_SVG } from './progress-bar.consts'
+
+export const useProgressBar = (scrollId, progressRef) => {
   const { scroll } = useLocomotiveScroll()
   const { colors } = theme
-  const heightPath = 945
 
   useEffect(() => {
     const halfHeightWindow = window.innerHeight / 2
-    const dashOffsetPath = parseInt(getComputedStyle(progressRef.current).strokeDashoffset, 10)
-    const points = document.querySelectorAll<HTMLElement>(`[data-progress-bar-point="${scrollId}"]`)
+    const dashOffsetPathOfProgressBarSVG = parseInt(
+      getComputedStyle(progressRef.current).strokeDashoffset,
+      10
+    )
+    const pointsOfProgressBarSVG = document.querySelectorAll<HTMLElement>(
+      `[data-progress-bar-point="${scrollId}"]`
+    )
     const renderProgress = () => {
-      points.forEach((point) => {
+      pointsOfProgressBarSVG.forEach((point) => {
         const { y: oyPiont } = point.getBoundingClientRect()
         point.style.fill = // eslint-disable-line no-param-reassign
           oyPiont - halfHeightWindow <= 0 ? colors.progressBar.progress : colors.progressBar.default
       })
-      const { y: oyPath } = progressRef.current.getBoundingClientRect()
-      const percentageComplete = ((oyPath - halfHeightWindow) / heightPath) * 100
-      const offsetUnit = (percentageComplete * dashOffsetPath) / 100
-      progressRef.current.style.strokeDashoffset = (dashOffsetPath - offsetUnit).toString() // eslint-disable-line no-param-reassign
+      const { y: oyPathOfProgressBarSVG } = progressRef.current.getBoundingClientRect()
+      const percentageComplete =
+        ((oyPathOfProgressBarSVG - halfHeightWindow) / HEIGHT_PATH_OF_PROGRESS_BAR_SVG) * 100
+      const offsetUnit = (percentageComplete * dashOffsetPathOfProgressBarSVG) / 100
+      progressRef.current.style.strokeDashoffset = ( // eslint-disable-line no-param-reassign
+        dashOffsetPathOfProgressBarSVG - offsetUnit
+      ).toString()
     }
     if (scroll) {
       scroll.on('scroll', () => {
