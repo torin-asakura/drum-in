dbb93dcd3bc54b72c831ef2dcc811f409a23c1b5
Ilya Frolov <absolem.dev@gmail.com>
2023-11-06 19:40:31 +0400

    feat(progress-bar): add path and points components

--- ui/progress-bar/src/progress-bar.component.tsx
+++ ui/progress-bar/src/progress-bar.component.tsx
@@ -1,15 +1,44 @@
 import React                from 'react'
 import { FC }               from 'react'
-import { useRef }           from 'react'
 
-import ProgressBarSVG       from './progress-bar'
+import { Box }              from '@ui/layout'
+
+import LogoPoint            from './components/logo-point.component'
+import Point                from './components/point.component'
+import ProgressPath         from './components/progress-path.component'
+import { DefaultPath }      from './components/default-path.component'
 import { ProgressBarProps } from './progress-bar.interface'
-import { useProgressBar }   from './use-progress-bar.hook'
 
-const ProgressBar: FC<ProgressBarProps> = ({ scrollId }) => {
-  const progressRef = useRef(null)
-  useProgressBar(scrollId, progressRef)
-  return <ProgressBarSVG scrollId={scrollId} ref={progressRef} />
-}
+const ProgressBar: FC<ProgressBarProps> = ({ scrollId }) => (
+  <Box position='relative'>
+    <Box position='absolute' top={0}>
+      <DefaultPath />
+    </Box>
+    <Box position='absolute' top={0}>
+      <ProgressPath />
+    </Box>
+    <Box position='absolute' top={0} left={10}>
+      <LogoPoint />
+    </Box>
+    <Box position='absolute' top={162} left={4}>
+      <Point />
+    </Box>
+    <Box position='absolute' top={318} left={5}>
+      <Point />
+    </Box>
+    <Box position='absolute' top={492} left={4}>
+      <Point />
+    </Box>
+    <Box position='absolute' top={674} left={5}>
+      <Point />
+    </Box>
+    <Box position='absolute' top={857} left={4}>
+      <Point />
+    </Box>
+    <Box position='absolute' top={970} left={13}>
+      <LogoPoint />
+    </Box>
+  </Box>
+)
 
 export { ProgressBar }
--- ui/progress-bar/src/progress-bar.interface.ts
+++ ui/progress-bar/src/progress-bar.interface.ts
@@ -1,3 +1,7 @@
 export interface ProgressBarProps {
   scrollId: string
 }
+
+type PositionKey = 'x' | 'y'
+
+export type Position = Record<PositionKey, number>
