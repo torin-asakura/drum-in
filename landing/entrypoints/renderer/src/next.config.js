59a3020d1193a922842fa26246d855a6355754c3
Nikita Elfimov <nelfimov@mail.ru>
2024-02-12 17:00:06 +0300

    fix: output

--- landing/entrypoints/renderer/src/next.config.js
+++ landing/entrypoints/renderer/src/next.config.js
@@ -1,11 +1,14 @@
+/**
+ * @type { import('next').NextConfig}
+ */
 module.exports = {
   experimental: {
     externalDir: true,
     swcFileReading: false,
     workerThreads: true,
-    outputStandalone: true,
     esmExternals: 'loose',
   },
+  output: 'standalone',
   webpack: (config) => {
     config.module.rules.push({
       test: /\.(woff|woff2|eot|ttf|otf)$/i,
