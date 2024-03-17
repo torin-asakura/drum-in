687acb89b448d89c9bc69b79c3af3e4676d6ae4c
Oksana Zhdanovich <oksss.vv@gmail.com>
2024-03-17 21:46:21 +0300

    refactor: background for hero & course program & teacher & footer

--- landing/pages/fifth-dimension-page/fragments/landing-program-fifth-dimension/src/program-fifth-dimension.component.tsx
+++ landing/pages/fifth-dimension-page/fragments/landing-program-fifth-dimension/src/program-fifth-dimension.component.tsx
@@ -54,8 +54,8 @@ const ProgramFifthDimensionBlock = ({ scrollId }) => {
                   wide: '-1000px center',
                 }}
                 backgroundSize='100%'
-                backgroundImage={`url(${program?.background?.sourceUrl})`}
-              >
+                backgroundImage={`url(${'https://wp.drumin.pro/wp-content/uploads/2023/03/progress-horizontal-section.png'})`}
+              >s
                 <Layout flexBasis={{ _: 0, tablet: 0, laptop: 2700, wide: 2000 }} flexShrink={0} />
                 <Box width={{ _: 0, tablet: 0, laptop: 2000, wide: 2612 }}>
                   <ProgramIcon width='100%' height='auto' />
