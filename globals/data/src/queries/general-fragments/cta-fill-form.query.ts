1830298752d9d5c044a6cd6d990e5c298c3ee44f
Oksana Zhdanovich <oksss.vv@gmail.com>
2024-03-20 16:19:43 +0300

    feat: initial general-fragments queries & hooks

---
+++ globals/data/src/queries/general-fragments/cta-fill-form.query.ts
@@ -0,0 +1,13 @@
+import { gql } from '../../__generated__'
+
+export const GET_CTA_FILL_FORM = gql(`
+query CtaFillForm{
+  generalFragments {
+    nodes {
+      commonFragments {
+        
+      }
+    }
+  }
+}
+`)
