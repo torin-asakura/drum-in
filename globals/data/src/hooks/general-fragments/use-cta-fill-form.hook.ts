31bcb75d5b0ebff476f8ab3dfa75ace8fd4f9297
Oksana Zhdanovich <oksss.vv@gmail.com>
2024-03-21 00:41:14 +0300

    refactor: use data for cta

--- globals/data/src/hooks/general-fragments/use-cta-fill-form.hook.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery }          from '@apollo/client'
-
-import { GET_CTA_FILL_FORM } from '../../queries'
-
-export const useCtaFillForm = () => {
-  const { data } = useQuery(GET_CTA_FILL_FORM)
-
-  return { data }
-}
