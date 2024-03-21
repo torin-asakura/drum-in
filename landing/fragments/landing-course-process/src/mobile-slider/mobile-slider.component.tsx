57763f71022035269bb683028f822ffc9650250b
Oksana Zhdanovich <oksss.vv@gmail.com>
2024-03-22 00:26:30 +0300

    refactor: get data for footer

--- landing/fragments/landing-course-process/src/mobile-slider/mobile-slider.component.tsx
+++ landing/fragments/landing-course-process/src/mobile-slider/mobile-slider.component.tsx
@@ -8,118 +8,117 @@ import { Slider }      from '@ui/slider'
 import { SwiperSlide } from '@ui/slider'
 import { Text }        from '@ui/text'
 
-const MobileSlider = ({ processEducation }) =>  (
-    <Column>
-      <Box display={['flex', 'flex', 'none']}>
-        <Layout flexBasis={[20, 30, 40]} flexShrink={0} />
-        <Slider
-          clName='course-process-slider'
-          spaceBetween={16}
-          slidesPerView='auto'
-          slidesOffsetAfter={16}
-        >
-          <SwiperSlide>
-            <Box
-              width={[300, 350, 430]}
-              height={[286, 330, 450]}
-              borderRadius={['big', 'extra', 'bigger']}
-              border={['normalSmokyWhite', 'normalSmokyWhite', 'mediumBoldSmokyWhite']}
-            >
+const MobileSlider = ({ processEducation }) => (
+  <Column>
+    <Box display={['flex', 'flex', 'none']}>
+      <Layout flexBasis={[20, 30, 40]} flexShrink={0} />
+      <Slider
+        clName='course-process-slider'
+        spaceBetween={16}
+        slidesPerView='auto'
+        slidesOffsetAfter={16}
+      >
+        <SwiperSlide>
+          <Box
+            width={[300, 350, 430]}
+            height={[286, 330, 450]}
+            borderRadius={['big', 'extra', 'bigger']}
+            border={['normalSmokyWhite', 'normalSmokyWhite', 'mediumBoldSmokyWhite']}
+          >
+            <Layout flexBasis={[20, 26, 32]} flexShrink={0} />
+            <Column>
               <Layout flexBasis={[20, 26, 32]} flexShrink={0} />
-              <Column>
-                <Layout flexBasis={[20, 26, 32]} flexShrink={0} />
-                <Box>
-                  <Text
-                    fontWeight='medium'
-                    fontSize={['large', 'moderate', 'increased']}
-                    lineHeight={['small', 'small', 'default']}
-                    color='text.smokyWhite'
-                  >
-                    {processEducation?.[0]?.title}
-                  </Text>
-                </Box>
-                <Layout flexGrow={3} />
-                <Box alignSelf='end' width={[200, 220, 0]} height={[107, 120, 0]}>
-                  <ImageBlock
-                    alt={processEducation?.[0]?.picture?.alt}
-                    src={processEducation?.[0]?.picture?.image?.node?.sourceUrl}
-                  />
-                </Box>
-                <Layout flexBasis={[20, 26, 32]} flexShrink={0} />
-              </Column>
+              <Box>
+                <Text
+                  fontWeight='medium'
+                  fontSize={['large', 'moderate', 'increased']}
+                  lineHeight={['small', 'small', 'default']}
+                  color='text.smokyWhite'
+                >
+                  {processEducation?.[0]?.title}
+                </Text>
+              </Box>
+              <Layout flexGrow={3} />
+              <Box alignSelf='end' width={[200, 220, 0]} height={[107, 120, 0]}>
+                <ImageBlock
+                  alt={processEducation?.[0]?.picture?.alt}
+                  src={processEducation?.[0]?.picture?.image?.node?.sourceUrl}
+                />
+              </Box>
               <Layout flexBasis={[20, 26, 32]} flexShrink={0} />
-            </Box>
-          </SwiperSlide>
-          <SwiperSlide>
-            <Box
-              width={[300, 350, 430]}
-              height={[286, 330, 450]}
-              borderRadius={['big', 'extra', 'bigger']}
-              backgroundColor='background.smokyWhite'
-              border={['normalSmokyWhite', 'normalSmokyWhite', 'mediumBoldSmokyWhite']}
-            >
+            </Column>
+            <Layout flexBasis={[20, 26, 32]} flexShrink={0} />
+          </Box>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Box
+            width={[300, 350, 430]}
+            height={[286, 330, 450]}
+            borderRadius={['big', 'extra', 'bigger']}
+            backgroundColor='background.smokyWhite'
+            border={['normalSmokyWhite', 'normalSmokyWhite', 'mediumBoldSmokyWhite']}
+          >
+            <Layout flexBasis={[20, 26, 32]} flexShrink={0} />
+            <Column>
               <Layout flexBasis={[20, 26, 32]} flexShrink={0} />
-              <Column>
-                <Layout flexBasis={[20, 26, 32]} flexShrink={0} />
-                <Box>
-                  <Text
-                    fontWeight='medium'
-                    fontSize={['large', 'moderate', 'increased']}
-                    lineHeight={['small', 'small', 'default']}
-                    color='text.blackAmber'
-                  >
-                    {processEducation?.[1]?.title}
-                  </Text>
-                </Box>
-                <Layout flexGrow={3} />
-                <Box alignSelf='end' width={[178, 198, 0]} height={[140, 155, 0]}>
-                  <ImageBlock
-                    alt={processEducation?.[1]?.picture?.alt}
-                    src={processEducation?.[1]?.picture?.image?.node?.sourceUrl}
-                  />
-                </Box>
-                <Layout flexBasis={[20, 26, 32]} flexShrink={0} />
-              </Column>
+              <Box>
+                <Text
+                  fontWeight='medium'
+                  fontSize={['large', 'moderate', 'increased']}
+                  lineHeight={['small', 'small', 'default']}
+                  color='text.blackAmber'
+                >
+                  {processEducation?.[1]?.title}
+                </Text>
+              </Box>
+              <Layout flexGrow={3} />
+              <Box alignSelf='end' width={[178, 198, 0]} height={[140, 155, 0]}>
+                <ImageBlock
+                  alt={processEducation?.[1]?.picture?.alt}
+                  src={processEducation?.[1]?.picture?.image?.node?.sourceUrl}
+                />
+              </Box>
               <Layout flexBasis={[20, 26, 32]} flexShrink={0} />
-            </Box>
-          </SwiperSlide>
-          <SwiperSlide>
-            <Box
-              width={[300, 350, 430]}
-              height={[286, 330, 450]}
-              borderRadius={['big', 'extra', 'bigger']}
-              border={['normalSmokyWhite', 'normalSmokyWhite', 'mediumBoldSmokyWhite']}
-            >
+            </Column>
+            <Layout flexBasis={[20, 26, 32]} flexShrink={0} />
+          </Box>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Box
+            width={[300, 350, 430]}
+            height={[286, 330, 450]}
+            borderRadius={['big', 'extra', 'bigger']}
+            border={['normalSmokyWhite', 'normalSmokyWhite', 'mediumBoldSmokyWhite']}
+          >
+            <Layout flexBasis={[20, 26, 32]} flexShrink={0} />
+            <Column>
               <Layout flexBasis={[20, 26, 32]} flexShrink={0} />
-              <Column>
-                <Layout flexBasis={[20, 26, 32]} flexShrink={0} />
-                <Box>
-                  <Text
-                    fontWeight='medium'
-                    fontSize={['large', 'moderate', 'increased']}
-                    lineHeight={['small', 'small', 'default']}
-                    color='text.smokyWhite'
-                  >
-                    {processEducation?.[2]?.title}
-                  </Text>
-                </Box>
-                <Layout flexGrow={3} />
-                <Box alignSelf='end' width={[112, 132, 0]} height={[150, 165, 0]}>
-                  <ImageBlock
-                    alt={processEducation?.[2]?.picture?.alt}
-                    src={processEducation?.[2]?.picture?.image?.node?.sourceUrl}
-                  />
-                </Box>
-                <Layout flexBasis={[20, 26, 32]} flexShrink={0} />
-              </Column>
+              <Box>
+                <Text
+                  fontWeight='medium'
+                  fontSize={['large', 'moderate', 'increased']}
+                  lineHeight={['small', 'small', 'default']}
+                  color='text.smokyWhite'
+                >
+                  {processEducation?.[2]?.title}
+                </Text>
+              </Box>
+              <Layout flexGrow={3} />
+              <Box alignSelf='end' width={[112, 132, 0]} height={[150, 165, 0]}>
+                <ImageBlock
+                  alt={processEducation?.[2]?.picture?.alt}
+                  src={processEducation?.[2]?.picture?.image?.node?.sourceUrl}
+                />
+              </Box>
               <Layout flexBasis={[20, 26, 32]} flexShrink={0} />
-            </Box>
-          </SwiperSlide>
-        </Slider>
-      </Box>
-      <Layout flexBasis={[24, 84, 163]} />
-    </Column>
-  )
-
+            </Column>
+            <Layout flexBasis={[20, 26, 32]} flexShrink={0} />
+          </Box>
+        </SwiperSlide>
+      </Slider>
+    </Box>
+    <Layout flexBasis={[24, 84, 163]} />
+  </Column>
+)
 
 export { MobileSlider }
--- landing/fragments/landing-footer/src/bottom-row/bottom-row.component.tsx
+++ landing/fragments/landing-footer/src/bottom-row/bottom-row.component.tsx
@@ -1,46 +1,41 @@
-import React         from 'react'
+import React                from 'react'
+import { FormattedMessage } from 'react-intl'
 
-import { Box }       from '@ui/layout'
-import { Layout }    from '@ui/layout'
-import { NextLink }  from '@ui/link'
-import { Link }      from '@ui/link'
-import { Text }      from '@ui/text'
+import { Box }              from '@ui/layout'
+import { Layout }           from '@ui/layout'
+import { NextLink }         from '@ui/link'
+import { Link }             from '@ui/link'
+import { Text }             from '@ui/text'
 
-import { useFooter } from '../data'
-
-const BottomRow = () => {
-  const footer = useFooter()?.fragmentNewItem?.footer
-
-  return (
-    <Box
-      width='100%'
-      flexDirection={['column', 'column', 'row']}
-      alignItems={['center', 'center', 'start']}
-      justifyContent='space-between'
-    >
-      <Box>
-        <NextLink path={footer?.urlLink}>
-          <Text
-            textTransform='none'
-            fontWeight='medium'
-            fontSize={['micro', 'medium', 'regular']}
-            lineHeight='medium'
-            color='text.transparentSmokyWhite'
-          >
-            {footer?.titleLink}
-          </Text>
-        </NextLink>
-      </Box>
-      <Layout flexBasis={12} display={['flex', 'flex', 'none']} />
-      <Box>
-        <Link href={`mailto:${footer?.email}`} target='_blank'>
-          <Text fontFamily='secondary' fontWeight='bold' fontSize={['medium', 'large', 'moderate']}>
-            {footer?.email}
-          </Text>
-        </Link>
-      </Box>
+const BottomRow = ({ footer }) => (
+  <Box
+    width='100%'
+    flexDirection={['column', 'column', 'row']}
+    alignItems={['center', 'center', 'end']}
+    justifyContent='space-between'
+  >
+    <Box>
+      <NextLink path={footer?.privacyPolicy}>
+        <Text
+          textTransform='none'
+          fontWeight='medium'
+          fontSize={['micro', 'medium', 'regular']}
+          lineHeight='medium'
+          color='text.transparentSmokyWhite'
+        >
+          <FormattedMessage id='footer.privacy_policy' />
+        </Text>
+      </NextLink>
     </Box>
-  )
-}
+    <Layout flexBasis={12} display={['flex', 'flex', 'none']} />
+    <Box>
+      <Link href={`mailto:${footer?.email}`} target='_blank'>
+        <Text fontFamily='secondary' fontWeight='bold' fontSize={['medium', 'large', 'moderate']}>
+          {footer?.email}
+        </Text>
+      </Link>
+    </Box>
+  </Box>
+)
 
 export { BottomRow }
