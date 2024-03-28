d4c4617a89c9c8f8ee962c19208bd1ed752acf12
Oksana Zhdanovich <oksss.vv@gmail.com>
2024-03-28 12:54:29 +0300

    refactor: getting background for program from data

--- globals/data/src/__generated__/gql.ts
+++ globals/data/src/__generated__/gql.ts
@@ -1,7 +1,6 @@
 /* eslint-disable */
-import * as types                            from './graphql'
-
-import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core'
+import * as types from './graphql';
+import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
 
 /**
  * Map of all GraphQL operations in the project.
@@ -14,15 +13,11 @@ import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/
  * Therefore it is highly recommended to use the babel or swc plugin for production.
  */
 const documents = {
-  '\n  query GetCourse($id: ID!) {\n    course(id: $id) {\n      title\n    }\n  }\n':
-    types.GetCourseDocument,
-  '\nquery IndividualCourse($id: ID!) {\nindividualCourse(id: $id) {\ncontent\ntitle\nindividualCourseData {\n\nprocess {\nstep {\ndescription\ntitle\n}\n}\n\nbackground {\ncourseProgram{node{sourceUrl}}\ndesktop {\nfooter {\nnode {\nsourceUrl\n}\n}\nhero {\nnode {\nsourceUrl\n}\n}\nteacher {\nnode {\nsourceUrl\n}\n}\n}\nmobile {\nfooter {\nnode {\nsourceUrl\n}\n}\nhero {\nnode {\nsourceUrl\n}\n}\nteacher {\nnode {\nsourceUrl\n}\n}\n}\n}\n\nprice {\nbonuses\ncta\ncourseLengthInMonths\ndiscount\nfullPrice\nliveTrainingsNumber\nmonthlyPrice\noldPrice\ntitle\nvideoTrainingsNumber\n}\nhero {\nbenefits {\ncontent\n}\nrunningLine\ncourseConditions {\ndescription\nnumber\n}\ncta\n}\n}\n}\n}\n':
-    types.IndividualCourseDocument,
-  '\n  query GetPreview($uri: String!) {\n    mediaItemBy(uri: $uri) {\n      sourceUrl\n    }\n  }\n':
-    types.GetPreviewDocument,
-  '\n  query GetSeo($id: ID!) {\n    page(id: $id) {\n      seo {\n        metaDesc\n        focuskw\n        opengraphType\n        opengraphTitle\n        opengraphDescription\n        opengraphUrl\n        opengraphSiteName\n        opengraphModifiedTime\n        opengraphSiteName\n        opengraphImage {\n          mediaDetails {\n            sizes {\n              height\n              width\n              name\n              sourceUrl\n            }\n          }\n        }\n        twitterDescription\n        twitterTitle\n        twitterImage {\n          sourceUrl\n        }\n        title\n        breadcrumbs {\n          text\n          url\n        }\n      }\n    }\n  }\n':
-    types.GetSeoDocument,
-}
+    "\n  query GetCourse($id: ID!) {\n    course(id: $id) {\n      title\n    }\n  }\n": types.GetCourseDocument,
+    "\nquery IndividualCourse($id: ID!) {\nindividualCourse(id: $id) {\ncontent\ntitle\nindividualCourseData {\n\nprocess {\nstep {\ndescription\ntitle\n}\n}\n\nbackground {\ncourseProgram{node{sourceUrl}}\ndesktop {\nfooter {\nnode {\nsourceUrl\n}\n}\nhero {\nnode {\nsourceUrl\n}\n}\nteacher {\nnode {\nsourceUrl\n}\n}\n}\nmobile {\nfooter {\nnode {\nsourceUrl\n}\n}\nhero {\nnode {\nsourceUrl\n}\n}\nteacher {\nnode {\nsourceUrl\n}\n}\n}\n}\n\nprice {\nbonuses\ncta\ncourseLengthInMonths\ndiscount\nfullPrice\nliveTrainingsNumber\nmonthlyPrice\noldPrice\ntitle\nvideoTrainingsNumber\n}\nhero {\nbenefits {\ncontent\n}\nrunningLine\ncourseConditions {\ndescription\nnumber\n}\ncta\n}\n}\n}\n}\n": types.IndividualCourseDocument,
+    "\n  query GetPreview($uri: String!) {\n    mediaItemBy(uri: $uri) {\n      sourceUrl\n    }\n  }\n": types.GetPreviewDocument,
+    "\n  query GetSeo($id: ID!) {\n    page(id: $id) {\n      seo {\n        metaDesc\n        focuskw\n        opengraphType\n        opengraphTitle\n        opengraphDescription\n        opengraphUrl\n        opengraphSiteName\n        opengraphModifiedTime\n        opengraphSiteName\n        opengraphImage {\n          mediaDetails {\n            sizes {\n              height\n              width\n              name\n              sourceUrl\n            }\n          }\n        }\n        twitterDescription\n        twitterTitle\n        twitterImage {\n          sourceUrl\n        }\n        title\n        breadcrumbs {\n          text\n          url\n        }\n      }\n    }\n  }\n": types.GetSeoDocument,
+};
 
 /**
  * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
@@ -36,36 +31,27 @@ const documents = {
  * The query argument is unknown!
  * Please regenerate the types.
  */
-export function gql(source: string): unknown
+export function gql(source: string): unknown;
 
 /**
  * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function gql(
-  source: '\n  query GetCourse($id: ID!) {\n    course(id: $id) {\n      title\n    }\n  }\n'
-): typeof documents['\n  query GetCourse($id: ID!) {\n    course(id: $id) {\n      title\n    }\n  }\n']
+export function gql(source: "\n  query GetCourse($id: ID!) {\n    course(id: $id) {\n      title\n    }\n  }\n"): (typeof documents)["\n  query GetCourse($id: ID!) {\n    course(id: $id) {\n      title\n    }\n  }\n"];
 /**
  * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function gql(
-  source: '\nquery IndividualCourse($id: ID!) {\nindividualCourse(id: $id) {\ncontent\ntitle\nindividualCourseData {\n\nprocess {\nstep {\ndescription\ntitle\n}\n}\n\nbackground {\ncourseProgram{node{sourceUrl}}\ndesktop {\nfooter {\nnode {\nsourceUrl\n}\n}\nhero {\nnode {\nsourceUrl\n}\n}\nteacher {\nnode {\nsourceUrl\n}\n}\n}\nmobile {\nfooter {\nnode {\nsourceUrl\n}\n}\nhero {\nnode {\nsourceUrl\n}\n}\nteacher {\nnode {\nsourceUrl\n}\n}\n}\n}\n\nprice {\nbonuses\ncta\ncourseLengthInMonths\ndiscount\nfullPrice\nliveTrainingsNumber\nmonthlyPrice\noldPrice\ntitle\nvideoTrainingsNumber\n}\nhero {\nbenefits {\ncontent\n}\nrunningLine\ncourseConditions {\ndescription\nnumber\n}\ncta\n}\n}\n}\n}\n'
-): typeof documents['\nquery IndividualCourse($id: ID!) {\nindividualCourse(id: $id) {\ncontent\ntitle\nindividualCourseData {\n\nprocess {\nstep {\ndescription\ntitle\n}\n}\n\nbackground {\ncourseProgram{node{sourceUrl}}\ndesktop {\nfooter {\nnode {\nsourceUrl\n}\n}\nhero {\nnode {\nsourceUrl\n}\n}\nteacher {\nnode {\nsourceUrl\n}\n}\n}\nmobile {\nfooter {\nnode {\nsourceUrl\n}\n}\nhero {\nnode {\nsourceUrl\n}\n}\nteacher {\nnode {\nsourceUrl\n}\n}\n}\n}\n\nprice {\nbonuses\ncta\ncourseLengthInMonths\ndiscount\nfullPrice\nliveTrainingsNumber\nmonthlyPrice\noldPrice\ntitle\nvideoTrainingsNumber\n}\nhero {\nbenefits {\ncontent\n}\nrunningLine\ncourseConditions {\ndescription\nnumber\n}\ncta\n}\n}\n}\n}\n']
+export function gql(source: "\nquery IndividualCourse($id: ID!) {\nindividualCourse(id: $id) {\ncontent\ntitle\nindividualCourseData {\n\nprocess {\nstep {\ndescription\ntitle\n}\n}\n\nbackground {\ncourseProgram{node{sourceUrl}}\ndesktop {\nfooter {\nnode {\nsourceUrl\n}\n}\nhero {\nnode {\nsourceUrl\n}\n}\nteacher {\nnode {\nsourceUrl\n}\n}\n}\nmobile {\nfooter {\nnode {\nsourceUrl\n}\n}\nhero {\nnode {\nsourceUrl\n}\n}\nteacher {\nnode {\nsourceUrl\n}\n}\n}\n}\n\nprice {\nbonuses\ncta\ncourseLengthInMonths\ndiscount\nfullPrice\nliveTrainingsNumber\nmonthlyPrice\noldPrice\ntitle\nvideoTrainingsNumber\n}\nhero {\nbenefits {\ncontent\n}\nrunningLine\ncourseConditions {\ndescription\nnumber\n}\ncta\n}\n}\n}\n}\n"): (typeof documents)["\nquery IndividualCourse($id: ID!) {\nindividualCourse(id: $id) {\ncontent\ntitle\nindividualCourseData {\n\nprocess {\nstep {\ndescription\ntitle\n}\n}\n\nbackground {\ncourseProgram{node{sourceUrl}}\ndesktop {\nfooter {\nnode {\nsourceUrl\n}\n}\nhero {\nnode {\nsourceUrl\n}\n}\nteacher {\nnode {\nsourceUrl\n}\n}\n}\nmobile {\nfooter {\nnode {\nsourceUrl\n}\n}\nhero {\nnode {\nsourceUrl\n}\n}\nteacher {\nnode {\nsourceUrl\n}\n}\n}\n}\n\nprice {\nbonuses\ncta\ncourseLengthInMonths\ndiscount\nfullPrice\nliveTrainingsNumber\nmonthlyPrice\noldPrice\ntitle\nvideoTrainingsNumber\n}\nhero {\nbenefits {\ncontent\n}\nrunningLine\ncourseConditions {\ndescription\nnumber\n}\ncta\n}\n}\n}\n}\n"];
 /**
  * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function gql(
-  source: '\n  query GetPreview($uri: String!) {\n    mediaItemBy(uri: $uri) {\n      sourceUrl\n    }\n  }\n'
-): typeof documents['\n  query GetPreview($uri: String!) {\n    mediaItemBy(uri: $uri) {\n      sourceUrl\n    }\n  }\n']
+export function gql(source: "\n  query GetPreview($uri: String!) {\n    mediaItemBy(uri: $uri) {\n      sourceUrl\n    }\n  }\n"): (typeof documents)["\n  query GetPreview($uri: String!) {\n    mediaItemBy(uri: $uri) {\n      sourceUrl\n    }\n  }\n"];
 /**
  * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function gql(
-  source: '\n  query GetSeo($id: ID!) {\n    page(id: $id) {\n      seo {\n        metaDesc\n        focuskw\n        opengraphType\n        opengraphTitle\n        opengraphDescription\n        opengraphUrl\n        opengraphSiteName\n        opengraphModifiedTime\n        opengraphSiteName\n        opengraphImage {\n          mediaDetails {\n            sizes {\n              height\n              width\n              name\n              sourceUrl\n            }\n          }\n        }\n        twitterDescription\n        twitterTitle\n        twitterImage {\n          sourceUrl\n        }\n        title\n        breadcrumbs {\n          text\n          url\n        }\n      }\n    }\n  }\n'
-): typeof documents['\n  query GetSeo($id: ID!) {\n    page(id: $id) {\n      seo {\n        metaDesc\n        focuskw\n        opengraphType\n        opengraphTitle\n        opengraphDescription\n        opengraphUrl\n        opengraphSiteName\n        opengraphModifiedTime\n        opengraphSiteName\n        opengraphImage {\n          mediaDetails {\n            sizes {\n              height\n              width\n              name\n              sourceUrl\n            }\n          }\n        }\n        twitterDescription\n        twitterTitle\n        twitterImage {\n          sourceUrl\n        }\n        title\n        breadcrumbs {\n          text\n          url\n        }\n      }\n    }\n  }\n']
+export function gql(source: "\n  query GetSeo($id: ID!) {\n    page(id: $id) {\n      seo {\n        metaDesc\n        focuskw\n        opengraphType\n        opengraphTitle\n        opengraphDescription\n        opengraphUrl\n        opengraphSiteName\n        opengraphModifiedTime\n        opengraphSiteName\n        opengraphImage {\n          mediaDetails {\n            sizes {\n              height\n              width\n              name\n              sourceUrl\n            }\n          }\n        }\n        twitterDescription\n        twitterTitle\n        twitterImage {\n          sourceUrl\n        }\n        title\n        breadcrumbs {\n          text\n          url\n        }\n      }\n    }\n  }\n"): (typeof documents)["\n  query GetSeo($id: ID!) {\n    page(id: $id) {\n      seo {\n        metaDesc\n        focuskw\n        opengraphType\n        opengraphTitle\n        opengraphDescription\n        opengraphUrl\n        opengraphSiteName\n        opengraphModifiedTime\n        opengraphSiteName\n        opengraphImage {\n          mediaDetails {\n            sizes {\n              height\n              width\n              name\n              sourceUrl\n            }\n          }\n        }\n        twitterDescription\n        twitterTitle\n        twitterImage {\n          sourceUrl\n        }\n        title\n        breadcrumbs {\n          text\n          url\n        }\n      }\n    }\n  }\n"];
 
 export function gql(source: string) {
-  return (documents as any)[source] ?? {}
+  return (documents as any)[source] ?? {};
 }
 
-export type DocumentType<TDocumentNode extends DocumentNode<any, any>> =
-  TDocumentNode extends DocumentNode<infer TType, any> ? TType : never
+export type DocumentType<TDocumentNode extends DocumentNode<any, any>> = TDocumentNode extends DocumentNode<  infer TType,  any>  ? TType  : never;
