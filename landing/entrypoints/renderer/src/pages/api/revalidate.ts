0fdfe0a8ad2e07dac2db1764ce623ec095db7ca8
Oksana Zhdanovich <oksss.vv@gmail.com>
2024-04-11 18:32:09 +0300

    refactor: forced revalidation for all pages & change token env

--- landing/entrypoints/renderer/src/pages/api/revalidate.ts
+++ landing/entrypoints/renderer/src/pages/api/revalidate.ts
@@ -1,11 +1,22 @@
 export default async function handler(req, res) {
-  if (parseInt(req.query.secret) !== 987654321) {
+  if (req.query.secret !== process.env.REVALIDATE_DATA_TOKEN) {
     return res.status(401).json({ message: 'Invalid token' })
   }
 
   try {
     await res.revalidate('/')
+    await res.revalidate('/opening-the-rhythm')
+    await res.revalidate('/feeling-of-time')
+    await res.revalidate('/fifth-dimension')
+    await res.revalidate('/seventh-heaven')
+    await res.revalidate('/connacol')
+    await res.revalidate('/polyrhythmic-keys')
+    await res.revalidate('/contact')
+    await res.revalidate('/privacy-policy')
+    await res.revalidate('/contract-offer')
+
     return res.json({ revalidated: true })
+
   } catch (err) {
     return res.status(500).send('Error revalidating')
   }
