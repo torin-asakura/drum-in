27605051e2fcf8850ea168f8b09821dcf73244a7
Oksana Zhdanovich <oksss.vv@gmail.com>
2024-05-03 15:43:11 +0300

    refactor: seo query for all pages

--- landing/pages/contact-page/src/get-static-props.ts
+++ landing/pages/contact-page/src/get-static-props.ts
@@ -1,3 +1,4 @@
+import { MediaUrl }          from '@globals/data'
 import { GET_FOOTER }        from '@globals/data'
 import { GET_HEADER }        from '@globals/data'
 import { GeneralFragmentID } from '@globals/data'
@@ -18,11 +19,17 @@ export const getStaticProps = async () => {
 
   const { data: seoData } = await client.query({
     query: GET_SEO,
-    variables: { id: PageID.CONTACT },
+    variables: {
+      id: PageID.CONTACT,
+      uriDefaultIcon: MediaUrl.DEFAULT_ICON,
+      uriAppleIcon: MediaUrl.APPLE_ICON,
+    },
   })
 
   const SEO = {
-    ...seoData?.page?.seo,
+    ...seoData.page?.seo,
+    defaultIcon: seoData.defaultIcon?.mediaItemUrl,
+    appleIcon: seoData.appleIcon?.mediaItemUrl,
     ogLocale: 'ru_RU',
     twitterCard: 'summary_large_image',
   }
--- landing/pages/privacy-policy-page/src/get-static-props.ts
+++ landing/pages/privacy-policy-page/src/get-static-props.ts
@@ -1,3 +1,4 @@
+import { MediaUrl }           from '@globals/data'
 import { GET_FOOTER }         from '@globals/data'
 import { GET_HEADER }         from '@globals/data'
 import { GeneralFragmentID }  from '@globals/data'
@@ -18,13 +19,20 @@ export const getStaticProps = async () => {
 
   const { data: seoData } = await client.query({
     query: GET_SEO,
-    variables: { id: PageID.PRIVACY_POLICY },
+    variables: {
+      id: PageID.PRIVACY_POLICY,
+      uriDefaultIcon: MediaUrl.DEFAULT_ICON,
+      uriAppleIcon: MediaUrl.APPLE_ICON,
+    },
   })
-
   const SEO = {
-    ...seoData?.page?.seo,
+    ...seoData.page?.seo,
+    defaultIcon: seoData.defaultIcon?.mediaItemUrl,
+    appleIcon: seoData.appleIcon?.mediaItemUrl,
     ogLocale: 'ru_RU',
+    twitterCard: 'summary_large_image',
   }
+
   const { data: header } = await client.query({
     query: GET_HEADER,
     variables: { id: GeneralFragmentID.HEADER },
